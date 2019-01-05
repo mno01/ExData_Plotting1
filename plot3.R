01421ba88c57347d954eedef701a9179e3321cd3
mno01 <42007873+mno01@users.noreply.github.com>
2019-01-04 16:11:14 -0800

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -26,12 +26,12 @@ FebSubset$DateTime <- as.POSIXct(paste(FebSubset$Date, FebSubset$Time))
 
 #plot3.png
 
-#png(file = 'plot3.png')
+png(file = 'plot3.png')
 
 plot(FebSubset$DateTime, FebSubset$Sub_metering_1, type = "l", ylab= "Energy sub metering", xlab = "")
 lines(FebSubset$DateTime, FebSubset$Sub_metering_2, col= "red")
 lines(FebSubset$DateTime, FebSubset$Sub_metering_3, col= "blue")
 legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lwd = c(1,1,1), col= c("black", "red", "blue"))
 
-#dev.off()
+dev.off()
 
